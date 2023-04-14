9b620d86336e8b20a2b315dce033b4c888cdf15a
Yvonne-Atieno <oyvonneatieno@gmail.com>
2023-04-14 06:28:12 +0300

    first commit

---
+++ question.py
@@ -0,0 +1,20 @@
+#Write a function that takes a list of numbers as input and returns a new list with all duplicate numbers removed.
+
+def removedups(num):
+     return list(set(num))
+
+
+#Write a function that takes two numbers as input and returns their sum.
+def numbers(num1,num2):
+    return  num1 + num2
+
+
+#Write a function that takes a string as input and returns the length of the string.
+def string (money):
+     return length("money")
+     
+
+#Write a function that takes a list of numbers as input and returns the average of the numbers.
+numbers[4,9,7,6]
+def list(numbers):
+     return average(numbers)
